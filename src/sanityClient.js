7e1deabcb59ee5472b80808b7b136b50b5086355
Hunwoo Lee <ian9509@gmail.com>
2026-05-31 00:53:54 +0900

    fix sanity vite build error

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client'
 
 export const client = createClient({
-  projectId: '18oh8tdj',
-  dataset: 'production',
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset:   import.meta.env.VITE_SANITY_DATASET,
   useCdn: true,
   apiVersion: '2024-01-01',
 })
