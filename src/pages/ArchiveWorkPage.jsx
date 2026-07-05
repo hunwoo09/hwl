9260fb29b2a684aaaa0c085bf3af0bcc8ce30a92
hunwoo <admin@hunwoos-MacBook-Pro.local>
2026-07-06 04:07:58 +0900

    auto: update portfolio

--- src/pages/ArchiveWorkPage.jsx
+++ src/pages/ArchiveWorkPage.jsx
@@ -652,7 +652,7 @@ export default function ArchiveWorkPage() {
         <div
           ref={leftRef}
           style={{
-            position: 'absolute', top: 60, left: 0, right: 0,
+            position: 'absolute', top: 100, left: 0, right: 0,
             height: 72, display: 'flex', alignItems: 'center',
             padding: '0 40px', zIndex: 60, opacity: 0,
           }}
