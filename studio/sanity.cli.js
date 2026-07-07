b942700f4cbbe91f8fc269f3cf9b70e57e34e05e
hunwoo <admin@hunwoos-MacBook-Pro.local>
2026-07-07 17:30:07 +0900

    auto: update portfolio

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+    appId: 'klx0pzqzo2xs3cksr35902x1',
   }
 })
