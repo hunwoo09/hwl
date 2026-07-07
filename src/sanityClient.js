c839e0cfeb5e83484610b108b8ec1ef86bfa55db
hunwoo <admin@hunwoos-MacBook-Pro.local>
2026-07-07 23:39:28 +0900

    auto: update portfolio

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -3,6 +3,6 @@ import { createClient } from '@sanity/client'
 export const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset:   import.meta.env.VITE_SANITY_DATASET,
-  useCdn: true,
+  useCdn: !import.meta.env.DEV,
   apiVersion: '2024-01-01',
 })
